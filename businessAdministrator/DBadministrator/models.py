04d9dd764b36288f50f2f354f92174bbdd525951
Miguel Angel Bareño Bernal <miguel.bareno@hotmail.com>
2022-06-11 12:08:26 -0500

    model Bussines created

--- businessAdministrator/DBadministrator/models.py
+++ businessAdministrator/DBadministrator/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+from phonenumber_field.modelfields import PhoneNumberField
+class Business(models.Model):
+    company_name = models.CharField(max_length=50)
+    direction = models.CharField(max_length=20)
+    NIT = models.CharField(unique=True, max_length=10) #includes verification digit
+    phone = PhoneNumberField()  #cell phone or landline
