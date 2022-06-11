df739e9b4c2b1dd3a28d9767ed1fdbe8b472c812
Miguel Angel Bareño Bernal <miguel.bareno@hotmail.com>
2022-06-11 13:59:39 -0500

    databaseManagement template configuration

--- businessAdministrator/DBadministrator/views.py
+++ businessAdministrator/DBadministrator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
-def index(request):
-    return render(request, "databaseManagement.html")
+def databaseManagement(request):
+    return render(request, "DBadministrator/databaseManagement.html")
