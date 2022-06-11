55f8cdca5d9d7036f1b5e2bd0dd6b8caf4a8f3a4
Miguel Angel Bareño Bernal <miguel.bareno@hotmail.com>
2022-06-11 12:29:10 -0500

    metod __str__ Business model modification

--- businessAdministrator/DBadministrator/models.py
+++ businessAdministrator/DBadministrator/models.py
@@ -5,3 +5,7 @@ class Business(models.Model):
     direction = models.CharField(max_length=20)
     NIT = models.CharField(unique=True, max_length=10) #includes verification digit
     phone = PhoneNumberField()  #cell phone or landline
+
+    def __str__(self):
+        text = " company name :{0}  Nit: {1}"
+        return text.format(self.company_name, self.NIT)
